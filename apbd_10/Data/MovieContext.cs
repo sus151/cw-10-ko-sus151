6e0ce1d1354fd50422f62622a95434c846e8a84d
Jakub Sus <s22696@pjwstk.edu.pl>
2022-06-05 17:15:59 +0200

    Final commit

---
+++ apbd_10/Data/MovieContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using apbd_10.Models;
+
+namespace apbd_10.Data
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext (DbContextOptions<MovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
---
+++ apbd_10/Migrations/20220605150616_Dodanie kolumny Rating do tabeli Movie.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace apbd_10.Migrations
+{
+    public partial class DodaniekolumnyRatingdotabeliMovie : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Rating",
+                table: "Movie",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Movie");
+        }
+    }
+}
